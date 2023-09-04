15c34cf7df33cd491b3bd38c4fd3a02476065334
Boris Sea <seaborisrc@gmail.com>
2023-09-04 12:14:01 -0400

    Add controller to report

---
+++ ReportXLS/Controllers/ReportController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReportXLS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Report");
+        }
+    }
+}
